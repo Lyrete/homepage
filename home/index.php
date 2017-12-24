3042af2011aaa5e80a637b08fd437b09d35a78b3
Tommi <tommi@alajoki.fi>
2017-12-24 13:08:39 +0200

    Initial commit.

---
+++ home/index.php
@@ -0,0 +1,14 @@
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="windows-1250">
+        <title></title>
+    </head>
+    <body>
+        Hello.
+    </body>
+</html>
